a599e6ca5ac3b9efe5bd15b38d5ce030d8389f9a
licoby <yanzhao973@gmail.com>
2019-05-06 22:20:18 +0800

    fix #1: change url

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # vbo
-一个微博浏览器扩展
+一个微博浏览器扩展, [Chrome WebStore Site](https://chrome.google.com/webstore/detail/weiboer/hodlddkmellggjocbepiopigcfonccen)
 ## Feature
 - [x] 跳过推荐页
--- js/content.js
+++ js/content.js
@@ -1,7 +1,7 @@
 const skipGuide = () => {
   const currUrl = window.document.location.href
   if (currUrl === 'https://weibo.com/nguide/interests') {
-    window.document.location.href = 'https://weibo.com'
+    window.document.location.href = 'https://weibo.com/home'
   }
 }
 window.onload = () => {
